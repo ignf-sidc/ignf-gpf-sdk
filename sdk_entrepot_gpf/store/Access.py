77db3e9b2b76d0d81dc31aa2215783558da46ed6
Valentin Sasyan <valentin.sasyan@ign.fr>
2024-07-02 10:55:19 +0200

    fix(list) : Access listé comme le reste
    Attention cependant, on ne peux pas faire de filtres.

--- sdk_entrepot_gpf/store/Access.py
+++ sdk_entrepot_gpf/store/Access.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional, Type, Any
+from typing import Dict, Optional, Type, Any
 from sdk_entrepot_gpf.io.ApiRequester import ApiRequester
 
 from sdk_entrepot_gpf.store.StoreEntity import StoreEntity, T
@@ -34,44 +34,6 @@ class Access(StoreEntity):
         )
         # Instanciation
         return o_response.status_code == 204
-
-    @classmethod
-    def api_list(cls: Type[T], infos_filter: Optional[Dict[str, str]] = None, tags_filter: Optional[Dict[str, str]] = None, page: Optional[int] = None, datastore: Optional[str] = None) -> List[T]:
-        """Liste les accès de l'API respectant les paramètres donnés.
-
-        Args:
-            infos_filter: Filtres sur les attributs sous la forme `{"nom_attribut": "valeur_attribut"}`
-            tags_filter: Filtres sur les tags sous la forme `{"nom_tag": "valeur_tag"}`
-            page: Numéro page à récupérer, toutes si None.
-            datastore: Identifiant du datastore
-
-        Returns:
-            List[T]: liste des entités retournées
-        """
-        # Gestion des paramètres nuls
-        infos_filter = infos_filter if infos_filter is not None else {}
-        tags_filter = tags_filter if tags_filter is not None else {}
-
-        # Requête
-        o_response = ApiRequester().route_request("datastore_get", route_params={"datastore": datastore})
-
-        # Liste pour stocker les Access correspondants
-        l_access: List[T] = []
-
-        # Pour chaque access en dictionnaire
-        for d_access in o_response.json():
-            # On suppose qu'il est ok
-            b_ok = True
-            # On vérifie s'il respecte les critères d'attributs (car l'API ne permet pas de filter...)
-            for k, v in infos_filter.items():
-                if str(d_access.get(k)) != str(v):
-                    b_ok = False
-                    break
-            # S'il est ok au final, on l'ajoute
-            if b_ok:
-                l_access.append(cls(d_access))
-        # A la fin, on renvoie la liste
-        return l_access
 
     def api_update(self) -> None:
         return None
