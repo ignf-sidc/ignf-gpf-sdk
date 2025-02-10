4886ea89076693e09fc79519d09580f53f2d4485
Valentin Sasyan <valentin.sasyan@ign.fr>
2025-02-10 14:25:40 +0100

    fix(cli) : entities

--- sdk_entrepot_gpf/scripts/entities.py
+++ sdk_entrepot_gpf/scripts/entities.py
@@ -33,6 +33,25 @@ from sdk_entrepot_gpf.scripts.utils import Utils
 class Entities:
     """Classe pour manipuler les entités en cas d'utilisation cli."""
 
+    ENTITIES = [
+        Annexe,
+        Check,
+        CheckExecution,
+        Configuration,
+        Datastore,
+        Endpoint,
+        Key,
+        Metadata,
+        Offering,
+        Permission,
+        Processing,
+        ProcessingExecution,
+        Static,
+        StoredData,
+        Tms,
+        Upload,
+    ]
+
     def __init__(self, datastore: Optional[str], entity_type: str, idu: Optional[str], args: argparse.Namespace) -> None:  # pylint: disable=too-many-branches
         """Si un id est précisé, on récupère l'entité et on fait d'éventuelles actions.
         Sinon on liste les entités avec éventuellement des filtres.
@@ -270,54 +289,75 @@ class Entities:
 
     @staticmethod
     def complete_parser_entities(o_sub_parsers: argparse._SubParsersAction[argparse.ArgumentParser]) -> None:  # pylint: disable=too-many-statements,too-many-branches
-        """Complète le parser avec les sous-parsers pour les entités."""
-        # Parsers pour entités
-        l_entities = [
-            Annexe,
-            Check,
-            CheckExecution,
-            Configuration,
-            Datastore,
-            Endpoint,
-            Key,
-            Metadata,
-            Offering,
-            Permission,
-            Processing,
-            ProcessingExecution,
-            Static,
-            StoredData,
-            Tms,
-            Upload,
-        ]
-        for o_entity in l_entities:
-            # On génère la doc
+        """Complète le parser avec les sous-parsers pour chaque entité."""
+        # Pour chaque entité
+        for o_entity in Entities.ENTITIES:
+            # On crée le parseur
+            o_sub_parser = o_sub_parsers.add_parser(
+                f"{o_entity.entity_name()}",
+                help=f"Gestion des {o_entity.entity_titles()}",
+                formatter_class=argparse.RawTextHelpFormatter,
+            )
+            # Puis on génère la doc en ajoutant les paramètres
             l_epilog = []
             l_epilog.append("""Types de lancement :""")
+            # Id
+            o_sub_parser.add_argument("id", type=str, nargs="?", default=None, help="Id de l'entité à afficher ou à utiliser pour lancer les actions")
+            # Filtres
+            o_sub_parser.add_argument("--infos", "-i", type=str, default=None, help=f"Filtrer les {o_entity.entity_titles()} selon les infos")
+            o_sub_parser.add_argument("--page", "-p", type=int, default=None, help="Page à récupérer. Toutes si non indiqué.")
             if issubclass(o_entity, TagInterface):
-                l_epilog.append(f"""    * lister les {o_entity.entity_titles()} avec d'optionnels filtres sur les infos et les tags : {o_entity.entity_name()} [--infos INFOS] [--tags TAGS]""")
+                l_epilog.append(
+                    f"""    * lister les {o_entity.entity_titles()} avec d'optionnels filtres sur les infos et les tags : {o_entity.entity_name()} [--infos INFO=VALEUR] [--tags TAG=VALEUR]"""
+                )
+                o_sub_parser.add_argument("--tags", "-t", type=str, default=None, help=f"Filtrer les {o_entity.entity_titles()} selon les tags")
             else:
                 l_epilog.append(f"""    * lister les {o_entity.entity_titles()} avec d'optionnels filtres sur les infos : {o_entity.entity_name()} [--infos INFOS]""")
             l_epilog.append(f"""    * afficher le détail d'une entité : {o_entity.entity_name()} ID""")
             l_epilog.append("""    * effectuer une ACTION sur une entité :""")
             l_epilog.append(f"""        - suppression : {o_entity.entity_name()} ID --delete""")
             l_epilog.append(f"""        - suppression sans confirmation : {o_entity.entity_name()} ID --delete --force""")
+
             if o_entity == Annexe:
                 l_epilog.append(f"""    * publication / dépublication : `{o_entity.entity_name()} ID [--publish|--unpublish]`""")
+                o_sub_parser.add_argument("--publish", action="store_true", help="Publication de l'annexe (uniquement avec --id)")
+                o_sub_parser.add_argument("--unpublish", action="store_true", help="Dépublication de l'annexe (uniquement avec --id)")
                 l_epilog.append(f"""    * publication par label : `{o_entity.entity_name()} --publish-by-label label1,label2`""")
+                o_sub_parser.add_argument("--publish-by-label", type=str, default=None, help="Publication des annexes portant les labels donnés (ex: label1,label2)")
                 l_epilog.append(f"""    * dépublication par label : `{o_entity.entity_name()} --unpublish-by-label label1,label2`""")
+                o_sub_parser.add_argument("--unpublish-by-label", type=str, default=None, help="Dépublication des annexes portant les labels donnés (ex: label1,label2)")
+
             if o_entity == Endpoint:
                 l_epilog.append(f"""    * publication de métadonnée : `{o_entity.entity_name()} --publish-metadatas NOM_FICHIER`""")
+                o_sub_parser.add_argument("--publish-metadatas", type=str, default=None, help="Publication des métadonnées indiquées (ex: fichier_1,fichier_2)")
                 l_epilog.append(f"""    * dépublication de métadonnée : `{o_entity.entity_name()} --unpublish-metadatas NOM_FICHIER`""")
+                o_sub_parser.add_argument("--unpublish-metadatas", type=str, default=None, help="Dépublication des métadonnées indiquées (ex: fichier_1,fichier_2)")
+
             if o_entity == Key:
                 l_epilog.append("""    * création de clef : `--f FICHIER`\nExemple du contenu du fichier : `{"key": [{"name": "nom","type": "HASH","type_infos": {"hash": "mon_hash"}}]}`""")
+                o_sub_parser.add_argument("--file", "-f", type=str, default=None, help="Chemin vers le fichier décrivant les clefs à créer")
+
             if o_entity == Upload:
                 l_epilog.append(f"""        - ouverture : {o_entity.entity_name()} ID --open""")
+                o_sub_parser.add_argument("--open", action="store_true", default=False, help="Rouvrir une livraison fermée")
                 l_epilog.append(f"""        - fermeture : {o_entity.entity_name()} ID --close""")
+                o_sub_parser.add_argument("--close", action="store_true", default=False, help="Fermer une livraison ouverte")
                 l_epilog.append(f"""        - synthèse des vérifications : {o_entity.entity_name()} ID --checks""")
+                o_sub_parser.add_argument("--checks", action="store_true", default=False, help="Affiche le bilan des vérifications d'une livraison")
                 l_epilog.append(f"""        - suppression de fichiers téléversés : {o_entity.entity_name()} ID --delete-files FILE [FILE]""")
+                o_sub_parser.add_argument("--delete-files", type=str, default=False, help="Supprime les fichiers téléversés indiqués d'une livraison.")
                 l_epilog.append(f"""        - suppression auto des fichiers mal téléversés {o_entity.entity_name()} ID --delete-failed-files""")
+                o_sub_parser.add_argument("--delete-failed-files", action="store_true", default=False, help="Supprime les fichiers mal téléversés d'une livraison vérifiées et en erreur.")
                 l_epilog.append(f"""    * créer / mettre à jour une livraison (déprécié) : {o_entity.entity_name()} --file FILE [--behavior BEHAVIOR] [--check-before-close]""")
+                # TODO déprécié
+                o_sub_parser.add_argument("--file", "-f", type=str, default=None, help="(déprécié) Chemin vers le fichier descriptor dont on veut effectuer la livraison)")
+                # TODO déprécié
+                o_sub_parser.add_argument(
+                    "--check-before-close", action="store_true", default=False, help="(déprécié) Si on vérifie l'ensemble de la livraison avant de fermer la livraison (uniquement avec --file|-f)"
+                )
+                # TODO déprécié
+                o_sub_parser.add_argument("--behavior", "-b", choices=UploadAction.BEHAVIORS, default=None, help="(déprécié) Action à effectuer si la livraison existe déjà (uniquement avec -f)")
+
             l_epilog.append("""""")
             l_epilog.append("""Exemples :""")
             l_epilog.append(f"""    * Listing des {o_entity.entity_title()}s dont le nom contient 'D038' : {o_entity.entity_name()} --infos name=%D038%""")
@@ -326,49 +366,13 @@ class Entities:
             if o_entity == Upload:
                 l_epilog.append(f"""    * Réouverture d'une livraison : {o_entity.entity_name()} 576c85eb-6a2e-4d0c-a0c9-ddb83536e1dc --open""")
                 l_epilog.append(f"""    * Suppression d'un fichier : {o_entity.entity_name()} 576c85eb-6a2e-4d0c-a0c9-ddb83536e1dc --delete-file dossier/fichier.txt""")
-            # Puis le parser
-            o_sub_parser = o_sub_parsers.add_parser(
-                f"{o_entity.entity_name()}",
-                help=f"Gestion des {o_entity.entity_titles()}",
-                epilog="\n".join(l_epilog),
-                formatter_class=argparse.RawTextHelpFormatter,
-            )
-            o_sub_parser.add_argument("id", type=str, nargs="?", default=None, help="Id de l'entité afficher")
-            # Filtres
-            o_sub_parser.add_argument("--infos", "-i", type=str, default=None, help="Filtrer les livraisons selon les infos")
-            o_sub_parser.add_argument("--page", "-p", type=int, default=None, help="Page à récupérer. Toutes si non indiqué.")
-            if issubclass(o_entity, TagInterface):
-                o_sub_parser.add_argument("--tags", "-t", type=str, default=None, help="Filtrer les livraisons selon les tags")
-            # Actions
-            if o_entity == Annexe:
-                o_sub_parser.add_argument("--publish", action="store_true", help="Publication de l'annexe (uniquement avec --id)")
-                o_sub_parser.add_argument("--unpublish", action="store_true", help="Dépublication de l'annexe (uniquement avec --id)")
-                o_sub_parser.add_argument("--publish-by-label", type=str, default=None, help="Publication des annexes portant les labels donnés (ex: label1,label2)")
-                o_sub_parser.add_argument("--unpublish-by-label", type=str, default=None, help="Dépublication des annexes portant les labels donnés (ex: label1,label2)")
-            if o_entity == Endpoint:
-                o_sub_parser.add_argument("--publish-metadatas", type=str, default=None, help="Publication des métadonnées indiquées (ex: fichier_1,fichier_2)")
-                o_sub_parser.add_argument("--unpublish-metadatas", type=str, default=None, help="Dépublication des métadonnées indiquées (ex: fichier_1,fichier_2)")
+
+            # TODO déprécié
             if o_entity == Static:
-                # TODO déprécié
                 o_sub_parser.add_argument("--file", "-f", type=str, default=None, help="Chemin vers le fichier descriptor dont on veut effectuer la livraison)")
+            # TODO déprécié
             if o_entity == Metadata:
-                # TODO déprécié
                 o_sub_parser.add_argument("--file", "-f", type=str, default=None, help="Chemin vers le fichier de métadonnées que l'on veut téléverser)")
-            if o_entity == Key:
-                # TODO déprécié
-                o_sub_parser.add_argument("--file", "-f", type=str, default=None, help="Chemin vers le fichier décrivant les clefs à créer")
-            if o_entity == Upload:
-                o_exclusive = o_sub_parser.add_mutually_exclusive_group()
-                o_exclusive.add_argument("--open", action="store_true", default=False, help="Rouvrir une livraison fermée (uniquement avec --id)")
-                o_exclusive.add_argument("--close", action="store_true", default=False, help="Fermer une livraison ouverte (uniquement avec --id)")
-                o_exclusive.add_argument("--checks", action="store_true", default=False, help="Affiche le bilan des vérifications d'une livraison")
-                o_exclusive.add_argument("--delete-files", type=str, default=False, help="Supprime les fichiers téléversés indiqué d'une livraison.")
-                o_exclusive.add_argument("--delete-failed-files", action="store_true", default=False, help="Supprime les fichiers mal téléversés d'une livraison vérifiées et en erreur.")
-                # TODO déprécié
-                o_sub_parser.add_argument("--file", "-f", type=str, default=None, help="Chemin vers le fichier descriptor dont on veut effectuer la livraison)")
-                # TODO déprécié
-                o_sub_parser.add_argument(
-                    "--check-before-close", action="store_true", default=False, help="Si on vérifie l'ensemble de la livraison avant de fermer la livraison (uniquement avec --file|-f)"
-                )
-                # TODO déprécié
-                o_sub_parser.add_argument("--behavior", "-b", choices=UploadAction.BEHAVIORS, default=None, help="Action à effectuer si la livraison existe déjà (uniquement avec -f)")
+
+            # On met à jour l'épilogue suite à la génération de la doc
+            o_sub_parser.epilog = "\n".join(l_epilog)
